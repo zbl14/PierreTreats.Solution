b6318030ab0ca028b2183b63b1b70a0b86074513
zbl14 <ifthereisoneday@gmail.com>
2022-08-12 11:07:53 -0700

    configuration for Identity

--- PierreTreats/Startup.cs
+++ PierreTreats/Startup.cs
@@ -29,30 +29,30 @@ namespace PierreTreats
         .AddDbContext<PierreTreatsContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     
-      // services.AddIdentity<ApplicationUser, IdentityRole>()
-      //   .AddEntityFrameworkStores<PierreTreatsContext>()
-      //   .AddDefaultTokenProviders();
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+        .AddEntityFrameworkStores<PierreTreatsContext>()
+        .AddDefaultTokenProviders();
     
-      // services.Configure<IdentityOptions>(options =>
-      // {
-      //   options.Password.RequireDigit = false;
-      //   options.Password.RequiredLength = 0;
-      //   options.Password.RequireLowercase = false;
-      //   options.Password.RequireNonAlphanumeric = false;
-      //   options.Password.RequireUppercase = false;
-      //   options.Password.RequiredUniqueChars = 0;
-      // });
+      services.Configure<IdentityOptions>(options =>
+      {
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredUniqueChars = 0;
+      });
     }
 
     public void Configure(IApplicationBuilder app)
     {
       app.UseDeveloperExceptionPage();
 
-      // app.UseAuthentication();
+      app.UseAuthentication();
 
       app.UseRouting();
 
-      // app.UseAuthorization();
+      app.UseAuthorization();
 
       app.UseEndpoints(routes =>
       {
