097807719728b5a94ac6bdb9efd4a16db761cb7f
zbl14 <ifthereisoneday@gmail.com>
2022-08-12 10:59:08 -0700

    add CRUD, AddFlavor(), DeleteFlavor(), search functionality

--- PierreTreats/Controllers/TreatsController.cs
+++ PierreTreats/Controllers/TreatsController.cs
@@ -1,14 +1,126 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using PierreTreats.Models;
-using System.Collections.Generic;
 using System.Linq;
-// using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace PierreTreats.Controllers
 {
   public class TreatsController : Controller
   {
+    private readonly PierreTreatsContext _db;
 
+    public TreatsController(PierreTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index(string searchString)
+    {
+      ViewBag.PageTitle = "All Treats";
+      if (!String.IsNullOrEmpty(searchString))
+      {
+        var model = _db.Treats
+          .Where(Treat => Treat.Name.Contains(searchString))
+          .OrderBy(Treat => Treat.Name)
+          .ToList();
+        return View(model);
+      }
+      else
+      {
+        var model = _db.Treats
+          .OrderBy(Treat => Treat.Name)
+          .ToList();
+        return View(model);
+      }
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.PageTitle = "New Treat";
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisTreat = _db.Treats
+        .Include(treat => treat.TreatFlavors)
+        .ThenInclude(join =>join.Flavor)
+        .FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.PageTitle = "Details";
+      return View(thisTreat);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      ViewBag.PageTitle = "Edit Treat";
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Treat treat)
+    {
+      _db.Entry(treat).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddFlavor(int id)
+    {
+      ViewBag.PageTitle="Assign Flavor";
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+        _db.TreatFlavor.Add(new TreatFlavor() { FlavorId = FlavorId, TreatId = treat.TreatId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      ViewBag.PageTitle = "Remove Treat";
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteFlavor(int joinId)
+    {
+      var joinEntry = _db.TreatFlavor.FirstOrDefault(entry => entry.TreatFlavorId == joinId);
+      _db.TreatFlavor.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
